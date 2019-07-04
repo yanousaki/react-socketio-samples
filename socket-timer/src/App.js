af38f3cee3b92601bcac77441fba80d3da31a1f5
mecsantos <santos@wni.com>
2019-07-04 18:57:21 +0900

    complete code

--- socket-timer/src/App.js
+++ socket-timer/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { subscribeToTimer } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    subscribeToTimer((err, timestamp) => this.setState({
+      timestamp
+    }));
+  }
+
+  state = {
+    timestamp: 'no timestamp yet'
+  };
+
+  render() {
+    return (
+      <div className="App">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the timer value: {this.state.timestamp}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
