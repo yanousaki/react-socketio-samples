4d3259d575c3ca90eb528d0b4423c449e6e6dd1d
mecsantos <santos@wni.com>
2019-07-25 14:41:59 +0900

    change pattern of payload

--- socket-voice/publish2.py
+++ socket-voice/publish2.py
@@ -19,7 +19,16 @@ data={
 
 msg_type='jma_eq_ewfcst'                                              
 dt=datetime.now()
-message={"to":"all", "msg":{"type":msg_type, "t":dt.strftime('%Y-%m-%dT%H:%M:%SZ'), "data":data}}
+message={
+    "to":"all", 
+    "msg": {
+        "msg": {
+            "type":msg_type, 
+            "t":dt.strftime('%Y-%m-%dT%H:%M:%SZ'), 
+            "data":data
+        }
+    }
+}
 print(json.dumps(message))
 
 r=requests.post(url, json=message)
